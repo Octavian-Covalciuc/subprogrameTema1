530123f717722798dc58addebb34263a17e72273
Octavian Covalciuc <70703557+Octavian-Covalciuc@users.noreply.github.com>
2022-09-14 10:45:31 +0300

    Update and rename adunareFractii.py to adunareInmultireFractii.py

--- adunareInmultireFractii.py
+++ adunareInmultireFractii.py
@@ -2,3 +2,8 @@ def adunareFractii(x : list, y : list):
     return f'{x[0] * y[-1] + y[0] * x[-1]}/{y[-1] * x[-1]}'
 
 print(adunareFractii([1,4], [1,2]))
+
+def inmultireaFractii(x : list, y : list):
+    return f'{x[0] * y[0]}/{x[-1] * y[-1]}'
+
+print(inmultireaFractii([1,4], [1,2]))
